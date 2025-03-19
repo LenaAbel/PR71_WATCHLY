8720d1629b5adede6f9b7e48b198cd79611a5aca
maiunnaise <maiouwenneuh@gmail.com>
2025-03-19 23:51:33 +0100

    feat : full css on content-card component

--- watchly/src/app/content-card/content-card.component.ts
+++ watchly/src/app/content-card/content-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-content-card',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-card.component.css']
 })
 export class ContentCardComponent {
+  @Input() type! : string;
 
 }
