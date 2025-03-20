5d86bfbf57dd2262fbd25e9f835b5899547aa373
maiunnaise <maiouwenneuh@gmail.com>
2025-03-20 20:43:51 +0100

    feat : css content section + spec for tv and film

--- watchly/src/app/content-section/content-section.component.ts
+++ watchly/src/app/content-section/content-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content-section',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content-section.component.css']
 })
 export class ContentSectionComponent implements OnInit {
-
+  @Input() type !: string;
   constructor() { }
 
   ngOnInit(): void {
@@ -19,7 +19,8 @@ export class ContentSectionComponent implements OnInit {
     episodes: 18,
     duration: '40 min',
     language: 'English',
-    description: `The delicate balance between the rich city of Piltover and the seedy underbelly of Zaun. Tensions between these city-states boil over with the creation of "hextech", a way for any person to control magical energy in Piltover, and in Zaun, a new drug called "shimmer" transforms humans into monsters...The delicate balance between the rich city of Piltover and the seedy underbelly of Zaun. Tensions between these city-states boil over with the creation of "hextech", a way for any person to control magical energy in Piltover, and in Zaun, a new drug called "shimmer" transforms humans into monsters...The delicate balance between the rich city of Piltover and the seedy underbelly of Zaun. Tensions between these city-states boil over with the creation of "hextech", a way for any person to control magical energy in Piltover, and in Zaun, a new drug called "shimmer" transforms humans into monsters...The delicate balance between the rich city of Piltover and the seedy underbelly of Zaun. Tensions between these city-states boil over with the creation of "hextech", a way for any person to control magical energy in Piltover, and in Zaun, a new drug called "shimmer" transforms humans into monsters...The delicate balance between the rich city of Piltover and the seedy underbelly of Zaun. Tensions between these city-states boil over with the creation of "hextech", a way for any person to control magical energy in Piltover, and in Zaun, a new drug called "shimmer" transforms humans into monsters...`,
+    director :'Christian Linke',
+    description: 'The delicate balance between the rich city of Piltover and the seedy underbelly of Zaun. Tensions between these city-states boil over with the creation of "hextech", a way for any person to control magical energy in Piltover, and in Zaun, a new drug called "shimmer" transforms humans into monsters. The rivalry between the cities splits families and friends as Arcane brings life to the relationships that shape some of League of Legends" famous champions including Vi, Jinx, Caitlyn, Jayce, Viktor, and Ekko.',
     imageUrl: 'assets/arcane.jpg', // Replace with your image path
     rating: 4, // 1-5 scale
     genres: ['Action', 'Adventure', 'Animation', 'Drama', 'Fantasy']
