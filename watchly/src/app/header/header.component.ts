1c8120f265f1f411880070546cee9d4cef7dc008
M-IZO <marine.izoulet11@gmail.com>
2025-04-02 10:40:41 +0200

    Adds admin button to route in the header (only if admin)

--- watchly/src/app/header/header.component.ts
+++ watchly/src/app/header/header.component.ts
@@ -10,16 +10,19 @@ export class HeaderComponent implements OnInit {
   @Input() route!: string;
 
   token: string | null = null;
+  isAdmin: boolean = false;
 
   constructor(private router: Router) {}
   ngOnChanges(): void {
     this.token = localStorage.getItem('authToken');
     console.log(this.token);
     this.route = this.router.url.split('/')[1]; // e.g. "login", "register"
+    this.isAdmin = localStorage.getItem('isAdmin') === 'true';
   }
   ngOnInit(): void {
     this.token = localStorage.getItem('authToken');
     console.log(this.token);
+    this.isAdmin = localStorage.getItem('isAdmin') === 'true';
 
     this.route = this.router.url.split('/')[1]; // e.g. "login", "register"
   }
